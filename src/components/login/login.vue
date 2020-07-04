89cda826297474c17f6acffad37ecc7c0ba74219
Essay-Core <15622277298@163.com>
2020-07-04 19:39:43 +0800

    add vue project to github

---
+++ src/components/login/login.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="login-warp">
+    <el-form
+      class="login-form"
+      :label-position="top"
+      label-width="80px"
+      :model="formdata">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button
+        @click.prevent = "handleLogin()"
+        class="login-btn" type="primary">登录</el-button>
+    </el-form>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        data(){
+          return{
+            formdata: {
+              username:"",
+              password:""
+            }
+          }
+        },
+      methods:{
+          async handleLogin(){
+            //使异步操作代码看起来像同步代码
+            //找到异步操作
+            //在异步操作前添加await
+            //并且在当前函数添加async
+
+            // const res = await this.$http.post('login', this.formdata)
+            // const {
+            //   data,
+            //   meta:{msg,status}
+            // } = res.data
+            //如果用户没登录，url直接登录到home组件
+            //保存正确的用户token
+            // localStorage.getItem("token",date.token)
+            //消息通知框
+            this.$message({
+              message: '恭喜你，这是一条成功消息',
+              type: 'success'
+            });
+
+            //跳转到home 组件页面
+            this.$router.push({name:'home'})
+          }
+      }
+    }
+</script>
+
+<style scoped>
+.login-warp{
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+  .login-form {
+    width: 400px;
+    background-color: #ffffff;
+    border-radius: 5px;
+    padding: 30px;
+
+  }
+
+  .login-warp .login-btn{
+      width: 100%;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,33 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from "./vuex";
+//add
+import ElementUI from 'element-ui'
+//导入默认样式
+import './assets/css/reset.css'
+//引入css样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+import MyServerHttp from './plugins/http'
+
+//适用vue插件
+Vue.use(ElementUI)
+Vue.use(MyServerHttp)
+
+/*控制台log是否输出 */
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router, //对象简写
+  // render:(h)=>h(App) 简写，可替换一下两行代码
+  components: { App },
+  template: '<App/>'
+
+
+})
+
